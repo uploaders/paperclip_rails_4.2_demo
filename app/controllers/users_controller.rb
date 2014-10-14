ee4cd1f4015f836bc9ddefa4e9d2c3db8ab90212
Pete Mitchell <petemmitchell@gmail.com>
2014-10-13 20:13:35 -0700

    Demo now working and complete. Added .sample_env to help guide readers connect to AWS services without committing there private keys.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user
   end
 
   # POST /users
@@ -40,12 +41,10 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
     end
   end
 
